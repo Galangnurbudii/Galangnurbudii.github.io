0971d2ce1e269d503ffebcc5c2b876aee7f5f2ed
Galangnurbudii <88934659+Galangnurbudii@users.noreply.github.com>
2022-04-13 20:33:56 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-var video = document.getElementById('video');
-function printScroll(){
-    // console.log(document.documentElement.scrollTop)
-    if(document.documentElement.scrollTop > 800 && document.documentElement.scrollTop < 1450){
-        video.play()
-        
-    }
-    else {
-        video.pause()
-    }
+var video = document.getElementById("video");
+function printScroll() {
+	// console.log(document.documentElement.scrollTop)
+	if (
+		document.documentElement.scrollTop > 800 &&
+		document.documentElement.scrollTop < 1450
+	) {
+		video.play();
+	} else {
+		video.pause();
+	}
 }
 
-window.addEventListener('scroll',printScroll,false)
+window.addEventListener("scroll", printScroll, false);
 
-const backToTop = document.getElementById("backToTop")
-backToTop.addEventListener('click',function(){
-    window.scrollTo(0,0)
-})
+const backToTop = document.getElementById("backToTop");
+backToTop.addEventListener("click", function () {
+	window.scrollTo(0, 0);
+});
